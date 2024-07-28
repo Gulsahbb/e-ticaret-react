56cc6525eb97449f704bc7b9710b4f0e1daa59e3
Gulsahbb <gulsahbaskurt.10@gmail.com>
2024-07-29 01:31:28 +0300

    update

--- e-ticaret-app/src/component/Header.jsx
+++ e-ticaret-app/src/component/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import '../css/Header.css'
+import { CiShoppingBasket } from "react-icons/ci";
 function Header() {
     return (
-        <div>
-            Header
+        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+            <div className='flex-row'>
+                <img className='logo' src="./src/images/logo.png" />
+                <p className='logo-p'>ROSE</p>
+            </div>
+
+            <div>
+                <input className='search-input' placeholder='Search' type="text" />
+            </div>
         </div>
     )
 }

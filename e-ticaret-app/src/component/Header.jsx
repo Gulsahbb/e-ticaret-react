be4c9ff0e97e5ba0240d07e697287e465da963a2
Gulsahbb <gulsahbaskurt.10@gmail.com>
2024-07-29 01:58:16 +0300

    update

--- e-ticaret-app/src/component/Header.jsx
+++ e-ticaret-app/src/component/Header.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../css/Header.css'
 import { CiShoppingBasket } from "react-icons/ci";
+import { CiLight } from "react-icons/ci";
+import { IoIosMoon } from "react-icons/io";
 function Header() {
+    const [theme, setTheme] = useState(false);
+
+    const changeTheme = () => {
+        const root = document.getElementById("root");
+
+        if (theme) {
+            root.style.backgroundColor = "black";
+            root.style.color = "#fff";
+        } else {
+            root.style.backgroundColor = "#fff";
+            root.style.color = "black";
+        }
+        setTheme(!theme);
+    }
     return (
         <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
             <div className='flex-row'>
@@ -9,8 +25,12 @@ function Header() {
                 <p className='logo-p'>ROSE</p>
             </div>
 
-            <div>
+            <div className='flex-row'>
                 <input className='search-input' placeholder='Search' type="text" />
+                <div>
+                    {theme ? <IoIosMoon className='icon' onClick={changeTheme} /> : <CiLight className='icon' onClick={changeTheme} />}
+                    <CiShoppingBasket className='icon' />
+                </div>
             </div>
         </div>
     )
